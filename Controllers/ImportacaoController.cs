f689fd971ac22de614ef82477580ebb9886f0808
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-08-28 18:15:55 -0300

    feat: Criação de controller para lançamentos

--- Controllers/ImportacaoController.cs
+++ Controllers/ImportacaoController.cs
@@ -88,18 +88,6 @@ namespace Financeiro.Controllers
 
             return Json(lancamentos);
         }
-
-        public async Task<IActionResult> Listar()
-        {
-            var lancamentos = await _context.Lancamentos
-                .Include(l => l.Conta)
-                    .ThenInclude(c => c.Banco)
-                .OrderByDescending(l => l.DataPostagem)
-                .ToListAsync();
-
-            return View(lancamentos);
-        }
-
         private async Task<List<Lancamento>> ObterNovosLançamentos(List<Transaction> extrato, Conta? conta)
         {
             var novosLancamentos = new List<Lancamento>();
